23b2de077cd812f85afa250f6bff33e079e450c1
alvaro-j <alvarojaime360@gmail.com>
2022-05-02 20:34:57 -0300

    import Widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Widget from "./components/Widget";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<Widget />
+		</>
+	);
 };
 
 export default App;
