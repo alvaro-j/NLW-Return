d7d246ee0cae0f83d00e1e68427a09a08e3b9ca4
alvaro-j <alvarojaime360@gmail.com>
2022-05-03 20:32:10 -0300

    import Popover cokponent  and add code

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -2,18 +2,21 @@ import React from "react";
 
 import { ChatTeardropDots } from "phosphor-react";
 
+import { Popover } from "@headlessui/react";
+
 const Widget = () => {
 	return (
-		<div className="absolute bottom-5 right-5">
-			<button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+		<Popover className="absolute bottom-5 right-5">
+			<Popover.Panel>oi</Popover.Panel>
+			<Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
 				{/*groud class is to connect things and apply the same effects to the elements inside it, like hover*/}
 				<ChatTeardropDots className="w-6 h-6" />
 				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
 					{/*max-w-0 = max width equals zero*/}
 					Feedback
 				</span>
-			</button>
-		</div>
+			</Popover.Button>
+		</Popover>
 	);
 };
 
