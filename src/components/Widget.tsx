11a1762657dc50a71b2c79d1e5d7aae2c38c062d
alvaro-j <alvarojaime360@gmail.com>
2022-05-03 20:12:04 -0300

    add comments

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -6,8 +6,10 @@ const Widget = () => {
 	return (
 		<div className="absolute bottom-5 right-5">
 			<button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+				{/*groud class is to connect things and apply the same effects to the elements inside it, like hover*/}
 				<ChatTeardropDots className="w-6 h-6" />
 				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all">
+					{/*max-w-0 = max width equals zero*/}
 					Feedback
 				</span>
 			</button>
