3c2a496852e6b875e911c271b2e51842308576e3
alvaro-j <alvarojaime360@gmail.com>
2022-05-04 20:30:50 -0300

    add tags and classes

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const WidgetForm = () => {
-	return <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">WidgetForm</div>;
+	return (
+		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+			<header>
+				<span className="text-xl leading-6">Leave your feedback</span>
+			</header>
+			<p>keep it a buck bro</p>
+			<footer className="text-xs text-neutral-500">
+				Feito com ♥️ pela{" "}
+				<a className="underline underline-offset-2" target="_blank" href="https://rocketseat.com.br">
+					Rocketseat
+				</a>
+			</footer>
+		</div>
+	);
 };
 
 export default WidgetForm;
