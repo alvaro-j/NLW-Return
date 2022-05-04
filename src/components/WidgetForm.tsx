b3b8c9c6826f27ac9677ba510458e1164ebfa782
alvaro-j <alvarojaime360@gmail.com>
2022-05-04 19:55:20 -0300

    fix responsivity

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WidgetForm = () => {
-	return <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg">WidgetForm</div>;
+	return <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">WidgetForm</div>;
 };
 
 export default WidgetForm;
