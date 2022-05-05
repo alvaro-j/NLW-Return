481a04a63e893289f3774bcc3b5128486770770d
alvaro-j <alvarojaime360@gmail.com>
2022-05-05 10:38:27 -0300

    import imgs, create feedbackTypes objct and create div

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,6 +1,34 @@
 import React from "react";
 import CloseButton from "./CloseButton";
 
+import bugImgUrl from "../assets/bug.svg";
+import ideaImgUrl from "../assets/idea.svg";
+import thoughtImgUrl from "../assets/thought.svg";
+
+const feedbackTypes = {
+	BUG: {
+		title: "Problem",
+		image: {
+			source: bugImgUrl,
+			alt: "Insect image",
+		},
+	},
+	IDEA: {
+		title: "Idea",
+		image: {
+			source: ideaImgUrl,
+			alt: "Light bulb image",
+		},
+	},
+	OTHER: {
+		title: "Problem",
+		image: {
+			source: thoughtImgUrl,
+			alt: "Thought ballon image",
+		},
+	},
+};
+
 const WidgetForm = () => {
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,10 +36,14 @@ const WidgetForm = () => {
 				<span className="text-xl leading-6">Leave your feedback</span>
 				<CloseButton />
 			</header>
-			<p>keep it a buck bro</p>
+			<div className="flex py-8 w-full">a</div>
 			<footer className="text-xs text-neutral-500">
 				Feito com ♥️ pela{" "}
-				<a className="underline underline-offset-2" target="_blank" href="https://rocketseat.com.br">
+				<a
+					className="underline underline-offset-2"
+					target="_blank"
+					href="https://rocketseat.com.br"
+				>
 					Rocketseat
 				</a>
 			</footer>
