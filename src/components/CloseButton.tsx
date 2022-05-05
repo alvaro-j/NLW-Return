bb91ebca61bae68fe8d4efea8074ec94e8de3b2d
alvaro-j <alvarojaime360@gmail.com>
2022-05-05 10:13:54 -0300

    add base code, icon and classes

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Popover } from "@headlessui/react";
+import { X } from "phosphor-react";
+import React from "react";
 
 const CloseButton = () => {
-  return (
-    <button>CloseButton</button>
-  )
-}
+	return (
+		<Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100 transition-all" title="Close feedback form">
+			<X weight="bold" className="w-4 h-4"/>
+		</Popover.Button>
+	);
+};
 
-export default CloseButton
+export default CloseButton;
