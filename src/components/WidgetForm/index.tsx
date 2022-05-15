261645608a7ab370d33f5c67a2d6d42023da12e9
alvaro-j <alvarojaime360@gmail.com>
2022-05-15 11:25:53 -0300

    remove header

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -39,10 +39,7 @@ const WidgetForm = () => {
 	const [feedbackType, setFeedbackType] = React.useState<FeedbackType | null>(null);
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-			<header>
-				<span className="text-xl leading-6">Leave your feedback</span>
-				<CloseButton />
-			</header>
+			
 			{!feedbackType ? (
 				<FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
 			) : (
