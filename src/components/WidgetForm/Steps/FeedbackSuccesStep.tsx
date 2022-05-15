2043908f87dd5c303dffff8eedcb41019615927c
alvaro-j <alvarojaime360@gmail.com>
2022-05-15 11:12:07 -0300

    create Steps components

---
+++ src/components/WidgetForm/Steps/FeedbackSuccesStep.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackSuccesStep = () => {
+  return (
+    <div>FeedbackSuccesStep</div>
+  )
+}
+
+export default FeedbackSuccesStep
---
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackTypeStep = () => {
+  return (
+    <div>FeedbackTypeStep</div>
+  )
+}
+
+export default FeedbackTypeStep
