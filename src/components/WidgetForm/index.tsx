780a35e516b357501c3f88fb605bd10fbac47bfb
alvaro-j <alvarojaime360@gmail.com>
2022-05-15 11:10:13 -0300

    create new folder and move file

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import CloseButton from "./CloseButton";
+import CloseButton from "../CloseButton";
 
-import bugImgUrl from "../assets/bug.svg";
-import ideaImgUrl from "../assets/idea.svg";
-import thoughtImgUrl from "../assets/thought.svg";
+import bugImgUrl from "../../assets/bug.svg";
+import ideaImgUrl from "../../assets/idea.svg";
+import thoughtImgUrl from "../../assets/thought.svg";
 
 // stores img url and it's alt text 👇
 
@@ -41,20 +41,24 @@ const WidgetForm = () => {
 				<span className="text-xl leading-6">Leave your feedback</span>
 				<CloseButton />
 			</header>
-			<div className="flex py-8 gap-2 w-full">
-				{/* Object.entries return a array from a object */}
-				{Object.entries(feedbackTypes).map(([key, value]) => (
-					<button
-						key={key}
-						className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition-all"
-						type="button"
-						onClick={() => setFeedbackType(key as FeedbackType)}
-					>
-						<img src={value.image.source} alt={value.image.alt} />
-						<span>{value.title}</span>
-					</button>
-				))}
-			</div>
+			{!feedbackType ? (
+				<div className="flex py-8 gap-2 w-full">
+					{/* Object.entries return a array from a object */}
+					{Object.entries(feedbackTypes).map(([key, value]) => (
+						<button
+							key={key}
+							className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition-all"
+							type="button"
+							onClick={() => setFeedbackType(key as FeedbackType)}
+						>
+							<img src={value.image.source} alt={value.image.alt} />
+							<span>{value.title}</span>
+						</button>
+					))}
+				</div>
+			) : (
+				<p>hi mf</p>
+			)}
 			<footer className="text-xs text-neutral-500">
 				Made with ♥️ by{" "}
 				<a
