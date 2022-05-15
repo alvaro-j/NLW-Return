eb7b2d12302029393cda7dafb381faef22b54fc8
alvaro-j <alvarojaime360@gmail.com>
2022-05-15 10:53:07 -0300

    add type option to feedbackType state

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -34,7 +34,7 @@ const feedbackTypes = {
 type FeedbackType = keyof typeof feedbackTypes; // keyof returns the keys of the feedbackTypes object
 
 const WidgetForm = () => {
-	const [feedbackType, setFeedbackType] = React.useState(null);
+	const [feedbackType, setFeedbackType] = React.useState<FeedbackType | null>(null);
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 			<header>
