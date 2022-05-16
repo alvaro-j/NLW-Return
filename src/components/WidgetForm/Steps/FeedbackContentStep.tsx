a1fb1cd0760473dd6e26ebddabe7a0007fd77b1f
alvaro-j <alvarojaime360@gmail.com>
2022-05-16 09:41:11 -0300

    create const and add title to header

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,19 +3,18 @@ import { FeedbackType, feedbackTypes } from "..";
 import CloseButton from "../../CloseButton";
 
 interface FeedbackContentStepProps {
-  feedbackType: FeedbackType;
+	feedbackType: FeedbackType;
 }
 
-const FeedbackContentStep = ({feedbackType}:FeedbackContentStepProps) => {
+const FeedbackContentStep = ({ feedbackType }: FeedbackContentStepProps) => {
+	const feedbackTypeInfo = feedbackTypes[feedbackType];
 	return (
 		<>
 			<header>
-				<span className="text-xl leading-6">{feedbackType}</span>
+				<span className="text-xl leading-6">{feedbackTypeInfo.title}</span>
 				<CloseButton />
 			</header>
-			<div className="flex py-8 gap-2 w-full">
-			
-			</div>
+			<div className="flex py-8 gap-2 w-full"></div>
 		</>
 	);
 };
