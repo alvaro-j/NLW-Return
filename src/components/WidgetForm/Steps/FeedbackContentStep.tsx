fd271148cffe177a26ece72349683779027a0a52
alvaro-j <alvarojaime360@gmail.com>
2022-05-22 11:14:39 -0300

    create screenshot state

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -13,6 +13,7 @@ const FeedbackContentStep = ({
 	feedbackType,
 	onFeedbackRestartRequest,
 }: FeedbackContentStepProps) => {
+	const [screenshot, setScreenshot] = React.useState<string | null>(null);
 	const feedbackTypeInfo = feedbackTypes[feedbackType]; // sets the correct feedback type based on the object feedbackTypes
 	return (
 		<>
@@ -40,7 +41,7 @@ const FeedbackContentStep = ({
 					className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
 				/>
 				<footer className="flex gap-2 mt-2">
-				<ScreenshotButton />
+					<ScreenshotButton />
 					<button
 						type="submit"
 						className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
