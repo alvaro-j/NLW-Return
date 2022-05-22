b79b5025f4cb594b304e74fee2c0ed080947db80
alvaro-j <alvarojaime360@gmail.com>
2022-05-22 10:50:12 -0300

    create Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import { CircleNotch } from "phosphor-react";
+import React from "react";
+
+const Loading = () => {
+	return (
+		<div>
+			<CircleNotch width="bold" className="w-4 h-4" />
+		</div>
+	);
+};
+
+export default Loading;
