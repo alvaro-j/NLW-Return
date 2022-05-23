c6fee9b72c8d73f0922190a3847d8afbb04f881b
alvaro-j <alvarojaime360@gmail.com>
2022-05-23 10:22:47 -0300

    import FeedbackSuccesStep and change ternary

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import ideaImgUrl from "../../assets/idea.svg";
 import thoughtImgUrl from "../../assets/thought.svg";
 import FeedbackTypeStep from "./Steps/FeedbackTypeStep";
 import FeedbackContentStep from "./Steps/FeedbackContentStep";
+import FeedbackSuccesStep from "./Steps/FeedbackSuccesStep";
 
 // stores img url and it's alt text 👇
 
@@ -37,15 +38,28 @@ export type FeedbackType = keyof typeof feedbackTypes; // keyof returns the keys
 
 const WidgetForm = () => {
 	const [feedbackType, setFeedbackType] = React.useState<FeedbackType | null>(null);
+	const [feedbackSent, setFeedbackSent] = React.useState(false);
+
 	const handleRestartFeedback = () => {
 		setFeedbackType(null);
 	};
+
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-			{!feedbackType ? (
-				<FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+			{feedbackSent ? (
+				<FeedbackSuccesStep />
 			) : (
-				<FeedbackContentStep onFeedbackRestartRequest={handleRestartFeedback} feedbackType={feedbackType} />
+				<>
+					{!feedbackType ? ( // if feedbackType is false, it shows the screen to choose it
+						<FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+					) : (
+						<FeedbackContentStep
+							onFeedbackRestartRequest={handleRestartFeedback}
+							feedbackType={feedbackType}
+							onFeedbackSent={() => setFeedbackSent(true)}
+						/>
+					)}
+				</>
 			)}
 			<footer className="text-xs text-neutral-500">
 				Made with ♥️ by{" "}
