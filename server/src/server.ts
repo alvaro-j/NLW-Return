c1f8f2089a441261ecd765ab72ae43c00c86171b
alvaro-j <alvarojaime360@gmail.com>
2022-05-24 10:47:37 -0300

    change return status and change to async func

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-	console.log(req.body);
+app.post("/feedbacks", async (req, res) => {
+	// uses async cause it can be slow to send the data sometimes
+	const { type, comment, screenshot } = req.body; // gets the data from the object
 
-	return res.send("<h1>hello world</h1>");
+	const feedback = await prisma.feedback.create({
+		data: {
+			type,
+			comment,
+			screenshot,
+		},
+	});
+
+	return res.status(201).json({ data: feedback }); // 201 means that smth was created
 });
 
 app.listen(3333, () => {
