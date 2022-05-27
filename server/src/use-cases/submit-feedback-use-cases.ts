3d2bfe5c4ce37db2bd79dd5e1d8475070408320c
alvaro-j <alvarojaime360@gmail.com>
2022-05-27 10:48:08 -0300

    add create function

--- server/src/use-cases/submit-feedback-use-cases.ts
+++ server/src/use-cases/submit-feedback-use-cases.ts
@@ -9,8 +9,15 @@ interface SubmitFeedbackUseCaseRequest {
 
 export class SubmitFeedbackUseCase {
 	constructor(private feedbacksRepository: FeedbacksRepository) {}
-  
+
 	async execute(request: SubmitFeedbackUseCaseRequest) {
 		const { type, comment, screenshot } = request;
+
+		await this.feedbacksRepository.create({
+			type,
+			comment,
+			screenshot,
+		});
+    
 	}
 }
