d33952cf0b0339ca6424f17f28547f6cbea9bdb0
alvaro-j <alvarojaime360@gmail.com>
2022-05-27 11:03:59 -0300

    execute submitFeedbackUseCase function

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import { prisma } from "./prisma";
 import nodemailer from "nodemailer";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-cases";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 
 export const routes = express.Router();
 
@@ -19,20 +21,27 @@ routes.post("/feedbacks", async (req, res) => {
 	// uses async cause it can be slow to send the data sometimes
 	const { type, comment, screenshot } = req.body; // gets the data from the object
 
-	const feedback = 
+	const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+	const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository);
 
-	await transport.sendMail({
-		// it waits the email to be send before return
-		from: "Feedget Team <oi@feedget.com>",
-		to: "Álvaro Jaime <alvarojaime360@gmail.com>",
-		subject: "New feedback",
-		html: [
-			`<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-			`<p>Feedback type: ${type}</p>`,
-			`<p>Comment: ${comment}</p>`,
-			`</div>`,
-		].join("\n"),
+	await submitFeedbackUseCase.execute({
+		type,
+		comment,
+		screenshot,
 	});
 
-	return res.status(201).json({ data: feedback }); // 201 means that smth was created
+	// await transport.sendMail({
+	// 	// it waits the email to be send before return
+	// 	from: "Feedget Team <oi@feedget.com>",
+	// 	to: "Álvaro Jaime <alvarojaime360@gmail.com>",
+	// 	subject: "New feedback",
+	// 	html: [
+	// 		`<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+	// 		`<p>Feedback type: ${type}</p>`,
+	// 		`<p>Comment: ${comment}</p>`,
+	// 		`</div>`,
+	// 	].join("\n"),
+	// });
+
+	return res.status(201).send(); // 201 means that smth was created
 });
