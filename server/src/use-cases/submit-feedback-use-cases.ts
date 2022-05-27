ea309f69f7ac929cccef12b026bf1cda046f65a9
alvaro-j <alvarojaime360@gmail.com>
2022-05-27 10:45:38 -0300

    create constructor and import FeedbacksRepository

--- server/src/use-cases/submit-feedback-use-cases.ts
+++ server/src/use-cases/submit-feedback-use-cases.ts
@@ -1,4 +1,16 @@
 // the use-cases folder is the actions that the user can perform
+import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+
+interface SubmitFeedbackUseCaseRequest {
+	type: string;
+	comment: string;
+	screenshot?: string;
+}
+
 export class SubmitFeedbackUseCase {
+	constructor(private feedbacksRepository: FeedbacksRepository) {}
   
+	async execute(request: SubmitFeedbackUseCaseRequest) {
+		const { type, comment, screenshot } = request;
+	}
 }
