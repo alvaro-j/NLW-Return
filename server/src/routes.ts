3e8039685d3f1cbe0260eaeb31045ee30a04ac76
alvaro-j <alvarojaime360@gmail.com>
2022-05-27 10:31:12 -0300

    create use-cases folder and submit method

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -19,13 +19,7 @@ routes.post("/feedbacks", async (req, res) => {
 	// uses async cause it can be slow to send the data sometimes
 	const { type, comment, screenshot } = req.body; // gets the data from the object
 
-	const feedback = await prisma.feedback.create({
-		data: {
-			type,
-			comment,
-			screenshot,
-		},
-	});
+	const feedback = 
 
 	await transport.sendMail({
 		// it waits the email to be send before return
---
+++ server/src/use-cases/submit-feedback-use-cases.ts
@@ -0,0 +1,4 @@
+// the use-cases folder is the actions that the user can perform
+export class SubmitFeedbackUseCase {
+  
+}
