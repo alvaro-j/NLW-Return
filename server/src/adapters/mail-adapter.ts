14e9101480c578047466b2ca30fe1bde922a29ba
alvaro-j <alvarojaime360@gmail.com>
2022-05-29 10:15:21 -0300

    create adapters folder and interfaces

---
+++ server/src/adapters/mail-adapter.ts
@@ -0,0 +1,9 @@
+// the adapterss folder is for the services apart from the database
+export interface SendMailData {
+	subject: string;
+	body: string;
+}
+
+export interface MailAdapter {
+	sendMail: (data: SendMailData) => void;
+}
