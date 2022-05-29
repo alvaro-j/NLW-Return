b31d3a67f0f1714dc23cecae293d377ba2012ce0
alvaro-j <alvarojaime360@gmail.com>
2022-05-29 10:20:45 -0300

    remove nodemailer code from routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,16 +7,6 @@ import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedback
 
 export const routes = express.Router();
 
-const transport = nodemailer.createTransport({
-	// initial config to nodemailer(it sends email)
-	host: "smtp.mailtrap.io",
-	port: 2525,
-	auth: {
-		user: "26571281aac4e3",
-		pass: "42598ff95e76d8",
-	},
-});
-
 routes.post("/feedbacks", async (req, res) => {
 	// uses async cause it can be slow to send the data sometimes
 	const { type, comment, screenshot } = req.body; // gets the data from the object
@@ -29,19 +19,6 @@ routes.post("/feedbacks", async (req, res) => {
 		comment,
 		screenshot,
 	});
-
-	// await transport.sendMail({
-	// 	// it waits the email to be send before return
-	// 	from: "Feedget Team <oi@feedget.com>",
-	// 	to: "Álvaro Jaime <alvarojaime360@gmail.com>",
-	// 	subject: "New feedback",
-	// 	html: [
-	// 		`<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-	// 		`<p>Feedback type: ${type}</p>`,
-	// 		`<p>Comment: ${comment}</p>`,
-	// 		`</div>`,
-	// 	].join("\n"),
-	// });
 
 	return res.status(201).send(); // 201 means that smth was created
 });
