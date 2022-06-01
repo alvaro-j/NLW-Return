f548a85c296c251c15885be4274bb2416f4df9de
alvaro-j <alvarojaime360@gmail.com>
2022-06-01 11:05:43 -0300

    add more tests

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,14 +1,54 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
+// the spy functions observes when another function is called
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+	{ create: createFeedbackSpy },
+	{ sendMail: sendMailSpy }
+);
+
 // creates a test suite
 describe("Submit feedback", () => {
 	// the keyword "it" test a functionality
 	it("should be able to submit a feedback", async () => {
-		const submitFeedback = new SubmitFeedbackUseCase(
-			{ create: async () => {} },
-			{ sendMail: async () => {} }
-		);
+		await expect(
+			// expect is a PROMISE, that's why it uses an await
+			submitFeedback.execute({
+				type: "bug",
+				comment: "trash app bro",
+				screenshot: "data:image/png;base64,ji02j09jweq20ej09j",
+			})
+		).resolves.not.toThrow();
 
+		expect(createFeedbackSpy).toHaveBeenCalled();
+		expect(sendMailSpy).toHaveBeenCalled();
+	});
+
+	it("shouldn't be able to submit a feedback without type", async () => {
+		await expect(
+			// expect is a PROMISE, that's why it uses an await
+			submitFeedback.execute({
+				type: "",
+				comment: "trash app bro",
+				screenshot: "data:image/png;base64,ji02j09jweq20ej09j",
+			})
+		).rejects.toThrow();
+	});
+
+	it("shouldn't be able to submit a feedback without comment", async () => {
+		await expect(
+			// expect is a PROMISE, that's why it uses an await
+			submitFeedback.execute({
+				type: "bug",
+				comment: "",
+				screenshot: "data:image/png;base64,ji02j09jweq20ej09j",
+			})
+		).rejects.toThrow();
+	});
+
+	it("shouldn't be able to submit a feedback with an invalid screeshot format", async () => {
 		await expect(
 			// expect is a PROMISE, that's why it uses an await
 			submitFeedback.execute({
@@ -16,6 +56,6 @@ describe("Submit feedback", () => {
 				comment: "trash app bro",
 				screenshot: "hi.png",
 			})
-		).resolves.not.toThrow();
+		).rejects.toThrow();
 	});
 });
