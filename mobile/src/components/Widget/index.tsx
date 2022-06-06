3276eabc5c31c776896ba3c0cff01f9009692607
alvaro-j <alvarojaime360@gmail.com>
2022-06-06 20:24:19 -0300

    import and add ChatTeardropDots

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   return (
     <>
     <TouchableOpacity>
-      
+      <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} />
     </TouchableOpacity>
     </>
   );
