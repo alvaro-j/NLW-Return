b27e315c633052ea4b4e262a068d12944aaf5e21
alvaro-j <alvarojaime360@gmail.com>
2022-06-06 20:02:59 -0300

    import and use TouchableOpacity

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <>
+    <TouchableOpacity>
+      
+    </TouchableOpacity>
+    </>
   );
 }
