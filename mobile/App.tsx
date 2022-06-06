b2b7d59817149af46a3e130350416b4bfb424d5e
alvaro-j <alvarojaime360@gmail.com>
2022-06-06 19:59:30 -0300

    import useFonts and create const

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import AppLoading from "expo-app-loading"; // prevents the app from loading without the fonts
+import { useFonts, Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
 import { Widget } from "./src/components/Widget/Widget";
 import { theme } from "./src/theme";
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 	return (
 		<View
 			style={{
@@ -11,7 +22,7 @@ export default function App() {
 				backgroundColor: theme.colors.background,
 			}}
 		>
-      <Widget />
+			<Widget />
 			<StatusBar style="light" backgroundColor="transparent" translucent />
 		</View>
 	);
