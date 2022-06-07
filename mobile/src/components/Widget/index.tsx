e86cc4e39dec66eaeb83c1e0b8c85ba01c6a9b5c
alvaro-j <alvarojaime360@gmail.com>
2022-06-07 11:28:03 -0300

    add button style

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { ChatTeardropDots } from 'phosphor-react-native';
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from "phosphor-react-native";
 
-import { styles } from './styles';
-import { theme } from '../../theme';
+import { styles } from "./styles";
+import { theme } from "../../theme";
 
 export function Widget() {
-  return (
-    <>
-    <TouchableOpacity>
-      <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} />
-    </TouchableOpacity>
-    </>
-  );
+	return (
+		<>
+			<TouchableOpacity style={styles.button}>
+				<ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} weight="bold" />
+			</TouchableOpacity>
+		</>
+	);
 }
