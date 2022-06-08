e2b2da8fa0471cc90543dd6fa124c4dbfc80b63a
alvaro-j <alvarojaime360@gmail.com>
2022-06-08 09:23:00 -0300

    use BottomSheet, create open func, change export

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { ChatTeardropDots } from "phosphor-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
 import { theme } from "../../theme";
 
-export function Widget() {
+function Widget() {
+	const bottomSheetRef = React.useRef<BottomSheet>(null); // refers to the bottom sheet
+	const handleOpenBottomSheet = () => {
+		bottomSheetRef.current?.expand();
+	};
 	return (
 		<>
-			<TouchableOpacity style={styles.button}>
+			<TouchableOpacity style={styles.button} onPress={handleOpenBottomSheet}>
 				<ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} weight="bold" />
 			</TouchableOpacity>
+			<BottomSheet ref={bottomSheetRef} snapPoints={[1, 280]}></BottomSheet>
 		</>
 	);
 }
+
+export default gestureHandlerRootHOC(Widget);
