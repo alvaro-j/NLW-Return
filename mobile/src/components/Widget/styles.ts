3e5e64ee4f28259cc1df2ff63a7cd80026b9ed1f
alvaro-j <alvarojaime360@gmail.com>
2022-06-08 06:45:49 -0300

    add styles to button

--- mobile/src/components/Widget/styles.ts
+++ mobile/src/components/Widget/styles.ts
@@ -1,5 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper"
+
+import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
-  button: {}
+	button: {
+		width: 48, // it doesn't need to use "px"
+		height: 48,
+		borderRadius: 24,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: theme.colors.brand,
+		position: "absolute",
+		right: 16,
+		bottom: getBottomSpace() + 16,
+	},
 });
