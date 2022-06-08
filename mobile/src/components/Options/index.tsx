347fd0dcdf60ced66af2667f1727995aa229c349
alvaro-j <alvarojaime360@gmail.com>
2022-06-08 10:37:21 -0300

    add View for Option component

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { View } from "react-native";
+import { feedbackTypes } from "../../utils/feedbackTypes";
 import { Copyright } from "../Copyright";
+import { Option } from "../Option";
 
 import { styles } from "./styles";
 
 export function Options() {
 	return (
 		<View style={styles.container}>
+			<View style={styles.options}>
+				{Object.entries(feedbackTypes).map(
+					(
+						[key, value] // destructuring the array first
+					) => (
+						<Option key={key} image={value.image} title={value.title} />
+					)
+				)}
+			</View>
 			<Copyright />
 		</View>
 	);
