ee643258d06dcfa61740e0a0e3944d2642e13ab3
alvaro-j <alvarojaime360@gmail.com>
2022-06-08 06:51:08 -0300

    import gesture handler

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import AppLoading from "expo-app-loading"; // prevents the app from loading without the fonts
