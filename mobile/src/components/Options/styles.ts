592852c284fe27a508d70ba271184db42bd71590
alvaro-j <alvarojaime360@gmail.com>
2022-06-08 10:37:05 -0300

    add styles to options

--- mobile/src/components/Options/styles.ts
+++ mobile/src/components/Options/styles.ts
@@ -4,4 +4,10 @@ export const styles = StyleSheet.create({
 	container: {
 		alignItems: "center",
 	},
+	options: {
+		width: "100%",
+		marginBottom: 48,
+		flexDirection: "row",
+		justifyContent: 'center'
+	}
 });
