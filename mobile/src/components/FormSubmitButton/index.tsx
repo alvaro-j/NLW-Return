fc97d4baf7148fef6ab36385fea6cca0a3c4353b
alvaro-j <alvarojaime360@gmail.com>
2022-06-12 14:53:27 -0300

    base code done

--- mobile/src/components/FormSubmitButton/index.tsx
+++ mobile/src/components/FormSubmitButton/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from "react-native";
+import { theme } from "../../theme";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-export function FormSubmitButton() {
-  return (
-    <View style={styles.container}>
+interface Props extends TouchableOpacityProps {
+	isLoading: boolean;
+}
 
-    </View>
-  );
+export function FormSubmitButton({ isLoading, ...rest }: Props) {
+	return (
+		<TouchableOpacity style={styles.container} {...rest}>
+			{isLoading ? (
+				<ActivityIndicator color={theme.colors.brand} />
+			) : (
+				<Text style={styles.title}>Send feedback</Text>
+			)}
+		</TouchableOpacity>
+	);
 }
