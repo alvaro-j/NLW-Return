ce449b450de45f2d26976d6757473ab466b5c110
alvaro-j <alvarojaime360@gmail.com>
2022-06-12 14:38:20 -0300

    add interface, icons and style

--- mobile/src/components/ScreenshotButton/index.tsx
+++ mobile/src/components/ScreenshotButton/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, TouchableOpacity, Image } from "react-native";
+import { Camera, Trash } from "phosphor-react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { theme } from "../../theme";
 
-export function Screenshot() {
-  return (
-    <View style={styles.container}>
+interface Props {
+	screenshot: string | null;
+	onTakeShot: () => void;
+	onRemoveShot: () => void;
+}
 
-    </View>
-  );
+export function ScreenshotButton({ screenshot, onRemoveShot, onTakeShot }: Props) {
+	return (
+		<TouchableOpacity style={styles.container} onPress={screenshot ? onRemoveShot : onTakeShot}>
+			{screenshot ? (
+				<Trash
+					size={22}
+					color={theme.colors.text_secondary}
+					weight="fill"
+					style={styles.trashCanIcon}
+				/>
+			) : (
+				<Camera
+					size={24}
+					color={theme.colors.text_primary}
+					weight="bold"
+				/>
+			)}
+		</TouchableOpacity>
+	);
 }
