c3d4717120c1ab362c33612e91bf36d647b53fd5
alvaro-j <alvarojaime360@gmail.com>
2022-06-12 17:23:02 -0300

    Success component and style done

--- mobile/src/components/Success/index.tsx
+++ mobile/src/components/Success/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 
-import { styles } from './styles';
+import succesImg from "../../assets/success.png";
+
+import { Copyright } from "../Copyright";
+
+import { styles } from "./styles";
 
 export function Success() {
-  return (
-    <View style={styles.container}>
-
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Image source={succesImg} style={styles.image} />
+			<Text style={styles.title}>Thanks for the feedback!</Text>
+			<TouchableOpacity style={styles.button}>
+				<Text style={styles.buttonTitle}>Send another one</Text>
+			</TouchableOpacity>
+			<Copyright />
+		</View>
+	);
 }
