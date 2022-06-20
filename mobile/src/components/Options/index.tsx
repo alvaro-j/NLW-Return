cb7dbd2c92551ea88915cdc00fc44fad6b5cc721
alvaro-j <alvarojaime360@gmail.com>
2022-06-20 10:12:16 -0300

    create interface

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { feedbackTypes } from "../../utils/feedbackTypes";
+
 import { Copyright } from "../Copyright";
 import { Option } from "../Option";
 
+import { feedbackTypes } from "../../utils/feedbackTypes";
 import { styles } from "./styles";
+import { FeedbackType } from "../Widget";
 
-export function Options() {
+interface Props {
+	onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: Props) {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Leave your feedback</Text>
