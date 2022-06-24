c07e9e8d1341e9d11a0dca0dd3ac03b94a0d5153
alvaro-j <alvarojaime360@gmail.com>
2022-06-24 10:39:36 -0300

    add disabled to btn

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -88,7 +88,7 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
 					onRemoveShot={handleRemoveScreenshot}
 					screenshot={screenshot}
 				/>
-				<FormSubmitButton onPress={handleSubmitFeedback} isLoading={isSendingFeedback} />
+				<FormSubmitButton disabled={!comment} onPress={handleSubmitFeedback} isLoading={isSendingFeedback} />
 			</View>
 		</View>
 	);
