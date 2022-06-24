c0ddc540ad62405065a92430d455172f80118b9f
alvaro-j <alvarojaime360@gmail.com>
2022-06-24 10:52:15 -0300

    add link

--- mobile/src/components/Copyright/index.tsx
+++ mobile/src/components/Copyright/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Linking, Text, View } from "react-native";
 
 import { styles } from "./styles";
 
 export function Copyright() {
 	return (
 		<View>
-			<Text style={styles.text}>Made with ♥️ by Rocketseat</Text>
+			<Text style={styles.text}>
+				Made with ♥️ by{" "}
+				<Text style={styles.link} onPress={() => Linking.openURL("https://github.com/alvaro-j")}>Álvaro J.</Text>
+			</Text>
 		</View>
 	);
 }
